12d9b35dc595fc664f1607c7ca55984ab85b80f4
tatialveso <tatiialveso@gmail.com>
2022-12-02 07:32:08 -0300

    :sparkles: finish order routes

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,78 +1,89 @@
-import express from 'express'
-import OrderModel from '../models/order.model.js'
-import ProductModel from '../models/product.model.js'
+import express from "express";
+import OrderModel from "../models/order.model.js";
+import ProductModel from "../models/product.model.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
     try {
-        const getAllOrders = await OrderModel.find()
-        return res.status(200).json(getAllOrders)
+        const getAllOrders = await OrderModel.find();
+        return res.status(200).json(getAllOrders);
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({msg: 'something went wrong :('})
+        console.log(error);
+        return res.status(500).json({ msg: "something went wrong :(" });
     }
-})
+});
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
     try {
-        const { id } = req.params
-        const getOrderById = await OrderModel.findById(id).populate("products")
-        return res.status(200).json(getOrderById)
+        const { id } = req.params;
+        const getOrderById = await OrderModel.findById(id)
+            .populate("products")
+            .populate({
+                path: "products",
+                populate: {
+                    path: "product",
+                    model: "Product",
+                },
+            });
+        let total = 0;
+        getOrderById.products.forEach((order) => {
+            total += order.product.price * order.amount;
+        });
+        return res.status(200).json({ getOrderById, total: total });
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({msg: 'something went wrong :('})
+        console.log(error);
+        return res.status(500).json({ msg: "something went wrong :(" });
     }
-})
+});
 
-router.post('/insert', async (req, res) => {
+router.post("/insert", async (req, res) => {
     try {
-        const ids = req.body.products
-       
-        const createNewOrder = await OrderModel.create(
-            {
-                salesDate: req.body.salesDate,    
-                products: { ids }
-            }
-        )
-
-        await ProductModel.findByIdAndUpdate(
-            ids,
-            // sempre adiciona dentro da array
-            { $push: { orders: createNewOrder._id } }
-        )
-
-        return res.status(201).json(createNewOrder)
+        const createNewOrder = await OrderModel.create({
+            products: req.body.products,
+        });
+        createNewOrder.products.forEach(async (element) => {
+            await ProductModel.findByIdAndUpdate(
+                element.product,
+                {
+                    $push: {
+                        orders: createNewOrder._id,
+                    },
+                },
+                { new: true, runValidators: true }
+            );
+        });
+        return res.status(201).json(createNewOrder);
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({msg: 'something went wrong :('})
+        console.log(error);
+        return res.status(500).json({ msg: "something went wrong :(" });
     }
-})
+});
 
-router.put('/edit/:id', async (req, res) => {
+router.put("/edit/:id", async (req, res) => {
     try {
-        const { id } = req.params
+        const { id } = req.params;
         const updateOrderById = await OrderModel.findByIdAndUpdate(
             id,
             { ...req.body },
-            { new: true, runValidators: true}
-        )
-        return res.status(200).json(updateOrderById)
+            { new: true, runValidators: true }
+        );
+        return res.status(200).json(updateOrderById);
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({msg: 'something went wrong :('})
+        console.log(error);
+        return res.status(500).json({ msg: "something went wrong :(" });
     }
-})
+});
 
-router.delete('/delete/:id', async (req, res) => {
+router.delete("/delete/:id", async (req, res) => {
     try {
-        const { id } = req.params
-        await OrderModel.findByIdAndDelete(id)
-        return res.status(204).json({msg: 'order deleted!'})
+        const { id } = req.params;
+        await OrderModel.findByIdAndDelete(id);
+        return res.status(204).json({ msg: "order deleted!" });
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({msg: 'something went wrong :('})
+        console.log(error);
+        return res.status(500).json({ msg: "something went wrong :(" });
     }
-})
+});
 
-export default router
+export default router;
